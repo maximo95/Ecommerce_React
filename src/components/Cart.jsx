a65a5ecce67fd548bab77aac17b33a6b3d32c95b
maximo95 <maximollallire@gmail.com>
2022-06-24 17:15:57 -0300

    render

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,17 +1,22 @@
+import { useContext, useState,useEffect } from 'react';
 import { MiContexto } from '../context/CartContext';
-import {useContext, useEffect} from 'react';
-import { ListGroup } from 'react-bootstrap';
 
 export default function Cart() {
   const {PrecioTotalProductos,cart} = useContext (MiContexto);
+  console.log(PrecioTotalProductos)
+  const [render,setRender] = useState()
 
+  useEffect(() => {
+  setRender (PrecioTotalProductos())
+  }, [cart])
+  
   return (
     <>
       {
       cart.length > 0 ?
       /*cart.map(producto =><ListGroup key = {cart.index} producto = {producto}></ListGroup>*/
       cart.map((producto, id) => 
-        <div key={id}>{PrecioTotalProductos(cart)}</div>)
+        <div key={id}>{render}</div>)
 
       :
       <h2>"Tu carrito está vacío"</h2>
